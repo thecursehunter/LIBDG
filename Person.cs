456d34da6ce24260bee109bf44d519e13fecda04
thecursehunter <PhamQuangMinhMDCH@proton.me>
2024-10-20 11:35:37 +0700

    Initial Commit

---
+++ Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIBDG
+{
+    public abstract class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public void Login()
+        {
+            Console.WriteLine($"{Name} has logged in.");
+        }
+        //chứa các thuộc tính và phương thức chung giữa Member và Librarian. 
+    }
+}
